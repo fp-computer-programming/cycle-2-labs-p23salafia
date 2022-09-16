b8d351aa564b4b23456d22740bbb382b32685682
Jan <p23jsalafia@fairfieldprep.org>
2022-09-16 12:11:57 -0400

    Lab 2-4 Submission

---
+++ lab_2-4.py
@@ -0,0 +1,13 @@
+
+a=1
+b=2
+c=3
+x=2
+y=5
+chunk1=(3+4*x)/5
+chunk2=10*(y-5)*(a+b+c)**2/x
+chunk3=9*((4/x**2)+((9+x)/y))
+print(chunk1)
+print(chunk2)
+print(chunk3)
+print((chunk1)-(chunk2)+(chunk3))
