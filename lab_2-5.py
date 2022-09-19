2e085c5ce114d0aa94d6ae5ce1dada948626e4dd
Jan <p23jsalafia@fairfieldprep.org>
2022-09-19 12:04:15 -0400

    Lab 2-5 Submission

---
+++ lab_2-5.py
@@ -0,0 +1,28 @@
+x=21
+y=21
+z=21
+w=21
+t=21
+a=21
+i=21
+#Question 1
+x+=5        #x=x+2
+print(x)
+#Question 2
+y=y-3       #y-=3
+print(y)
+#Question 3
+z**=2       #z=z^2
+print(z)
+#Question 4
+w=w%2       #w%=2
+print(w)
+#Question 5
+t=t/5       #t/=5
+print(t)
+#Question 6
+a*=12       #a=a*12
+print(a)
+#Question 7
+i=i//7      #i//=7
+print(i)
