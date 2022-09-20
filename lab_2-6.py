25d5dbeb1ef3e9d314a5922eb9a9e8eb23c62560
Jan <p23jsalafia@fairfieldprep.org>
2022-09-20 13:37:44 -0400

    Lab 2-6 and 2-7 Submission

---
+++ lab_2-6.py
@@ -0,0 +1,24 @@
+a=5
+b=10
+c=0
+d=-4
+if a % 2 == 0:
+    print('a is even')
+else:
+    print('a is odd')
+
+if b % 2 == 0:
+    print('b is even')
+else:
+    print('b is odd')
+
+if c % 2 == 0:
+    print('c is even')
+else:
+    print('c is odd')
+
+if d % 2 == 0:
+    print('d is even')
+else:
+    print('d is odd')
+    print(d)
---
+++ lab_2-7.py
@@ -0,0 +1,51 @@
+a=5
+b=10
+c=0
+d=-4
+if a % 2 == 0:
+    print('a is even')
+else:
+    print('a is odd')
+
+if b % 2 == 0:
+    print('b is even')
+else:
+    print('b is odd')
+
+if c % 2 == 0:
+    print('c is even')
+else:
+    print('c is odd')
+
+if d % 2 == 0:
+    print('d is even')
+else:
+    print('d is odd')
+
+if a < -5:
+    print("a is less than 5")
+elif -5 <= a <= 5:
+    print('a is between -5 and 5')
+else:
+    print("a is greater than 5")
+
+if b < -5:
+    print("b is less than 5")
+elif -5 <= b <= 5:
+    print('b is between -5 and 5')
+else:
+    print("b is greater than 5")
+
+if c < -5:
+    print("c is less than 5")
+elif -5 <= c <= 5:
+    print('c is between -5 and 5')
+else:
+    print("c is greater than 5")
+
+if d < -5:
+    print("d is less than 5")
+elif -5 <= d <= 5:
+    print('d is between -5 and 5')
+else:
+    print("d is greater than 5")
